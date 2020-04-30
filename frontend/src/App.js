bad50da91fd12efe53d18adece20dc37e0a0c5f2
Otávio <otavio.pontes1103@gmail.com>
2020-04-30 16:38:28 -0300

    config frontend prettier eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-import React from "react";
-
-import "./App.css";
+import React from 'react';
+import './styles/global.css';
 
 function App() {
-  return (
-    <>
-      <h1>Miau</h1>
-    </>
-  );
+    return (
+        <>
+            <h1>Miau</h1>
+        </>
+    );
 }
 
 export default App;
